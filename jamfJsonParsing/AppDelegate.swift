39b4f8e5fb4c2497d768891886382657540dfcb1
Doyle, Mark(Information Technology Services) <doylema@eitsackgg7v.local>
2018-12-27 08:08:30 -0500

    Initial Commit

---
+++ jamfJsonParsing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  jamfJsonParsing
+//
+//  Created by Doyle, Mark(Information Technology Services) on 12/27/18.
+//  Copyright © 2018 Doyle, Mark(Information Technology Services). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
